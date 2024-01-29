9aa3f689da8fd15dc6dcecafba8f677f9d8bfb00
kwanghunlee <lkhun9311@naver.com>
2024-01-29 18:11:59 +0900

    first commit

--- src/test/java/com/ecommerce/service/ServiceApplicationTests.java
+++ src/test/java/com/ecommerce/service/ServiceApplicationTests.java
@@ -1,11 +1,13 @@
 package com.ecommerce.service;
 
+import org.assertj.core.internal.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServiceApplicationTests {
 
+    @IgnoreForBinding
     @Test
     void contextLoads() {
     }
